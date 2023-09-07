671a1ed9377c81d35eb93e56f074817138be6013
ZBelouchi <zachary.belouchi@gmail.com>
2023-09-07 14:20:59 -0400

    Added rough styles for loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import images from '../assets/images'
 
-export default function Loading() {
+export default function Loading({message}) {
     return (
-        <h1>LOADING...</h1>
+        <div className="loading">
+            <h1 className='loading__header'>LOADING</h1>
+            <img className='loading__icon' src={images.loading} alt="loading" />
+            <p className='loading__message'>{message}</p>
+        </div>
     )
 }
